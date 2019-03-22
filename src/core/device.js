f54528199bd74eb1bd5b95fdc1d2684bf20cf97c
lyl <695498118@qq.com>
2019-03-22 23:10:22 +0800

    device

--- src/core/device.js
+++ src/core/device.js
@@ -1,16 +1,25 @@
-// if (window.DeviceOrientationEvent) {
-//     window.addEventListener('deviceorientation', function (event) {
-//         var a = document.getElementById('alpha'),
-//             b = document.getElementById('beta'),
-//             g = document.getElementById('gamma'),
-//             alpha = event.alpha,
-//             beta = event.beta,
-//             gamma = event.gamma;
+//device
+let device = () => {
+  if (window.DeviceOrientationEvent) {
+    window.addEventListener(
+      "deviceorientation",
+      lister,
+      false
+    );
+  } else {
+    document.querySelector("body").innerHTML = "你的浏览器不支持陀螺仪";
+  }
+};
 
-//         a.innerHTML = Math.round(alpha);
-//         b.innerHTML = Math.round(beta);
-//         g.innerHTML = Math.round(gamma);
+let lister= (event)=> {
+    var a = document.getElementById("alpha"),
+    b = document.getElementById("beta"),
+    g = document.getElementById("gamma"),
+    alpha = event.alpha,
+    beta = event.beta,
+    gamma = event.gamma;
 
-//     }, false);
-// } else {
-//     document.querySelector('body').innerHTML = '你的浏览器不支持陀螺仪';
+  a.innerHTML = Math.round(alpha);
+  b.innerHTML = Math.round(beta);
+  g.innerHTML = Math.round(gamma);
+}
